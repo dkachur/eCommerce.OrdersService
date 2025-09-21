2c0ca609c6a8f368cef3ea4a1fedc4a8725417db
Daniil Kachur <daniilkachur1704@gmail.com>
2025-09-21 14:20:32 +0300

    feat(domain): add Order and OrderItem entities

---
+++ eCommerce.OrdersService.Domain/Entities/Order.cs
@@ -0,0 +1,42 @@
+﻿namespace eCommerce.OrdersService.Domain.Entities;
+
+public class Order
+{
+    public Guid OrderId { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public DateTime OrderDate { get; private set; }
+
+    public decimal TotalBill { get; private set; }
+
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    private List<OrderItem> _orderItems = [];
+
+    public static Order New(Guid userId, DateTime orderDate, decimal totalBill, List<OrderItem> orderItems)
+    {
+        return new()
+        {
+            OrderId = Guid.NewGuid(),
+            UserId = userId,
+            OrderDate = orderDate,
+            TotalBill = totalBill,
+            _orderItems = orderItems.ToList()
+        };
+    }
+
+    public static Order Restore(Guid orderId, Guid userId, DateTime orderDate, decimal totalBill, List<OrderItem> orderItems)
+    {
+        return new()
+        {
+            OrderId = orderId,
+            UserId = userId,
+            OrderDate = orderDate,
+            TotalBill = totalBill,
+            _orderItems = orderItems.ToList()
+        };
+    }
+
+    private Order() { }
+}
---
+++ eCommerce.OrdersService.Domain/Entities/OrderItem.cs
@@ -0,0 +1,36 @@
+﻿namespace eCommerce.OrdersService.Domain.Entities;
+
+public class OrderItem
+{
+    public Guid ProductId { get; private set; }
+
+    public decimal UnitPrice { get; private set; }
+
+    public int Quantity { get; private set; }
+
+    public decimal TotalPrice { get; private set; }
+
+    public static OrderItem New(Guid productId, decimal unitPrice, int quantity)
+    {
+        return new()
+        {
+            ProductId = productId,
+            UnitPrice = unitPrice,
+            Quantity = quantity,
+            TotalPrice = unitPrice * quantity
+        };
+    }
+
+    public static OrderItem Restore(Guid productId, decimal unitPrice, int quantity, decimal totalPrice)
+    {
+        return new()
+        {
+            ProductId = productId,
+            UnitPrice = unitPrice,
+            Quantity = quantity,
+            TotalPrice = totalPrice
+        };
+    }
+
+    private OrderItem() { }
+}
