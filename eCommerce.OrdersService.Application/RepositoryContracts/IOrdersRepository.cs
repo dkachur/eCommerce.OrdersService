e36c7757c497f2641ec4d455a5e1d3991c39c28d
Daniil Kachur <daniilkachur1704@gmail.com>
2025-09-22 22:42:18 +0300

    feat(application): add repository contract

---
+++ eCommerce.OrdersService.Application/RepositoryContracts/IOrdersRepository.cs
@@ -0,0 +1,68 @@
+﻿using eCommerce.OrdersService.Domain.Entities;
+
+namespace eCommerce.OrdersService.Application.RepositoryContracts;
+
+/// <summary>
+/// Represents data access logic for managing <see cref="Order"/> and <see cref="OrderItem"/> entities.
+/// </summary>
+public interface IOrdersRepository
+{
+    /// <summary>
+    /// Retrieves all orders from the storage.
+    /// </summary>
+    /// <returns>
+    /// A collection of all <see cref="Order"/> instances.
+    /// If the storage contains no orders, an empty collection is returned.
+    /// </returns>
+    Task<IEnumerable<Order>> GetOrdersAsync();
+
+    /// <summary>
+    /// Retrieves order with the specified ID.
+    /// </summary>
+    /// <param name="id">The unique identifier of the order.</param>
+    /// <returns>
+    /// A <see cref="Order"/> with the specified ID if found;
+    /// otherwise, <c>null</c>.
+    /// </returns>
+    Task<Order?> GetByIdAsync(Guid id);
+
+    /// <summary>
+    /// Retrieves all orders that contain order item with the specified <paramref name="productId"/>.
+    /// </summary>
+    /// <param name="productId">The unique identifier of the product.</param>
+    /// <returns>
+    /// A collection of <see cref="Order"/> instances that match the search criteria.  
+    /// If no orders are found, an empty collection is returned.
+    /// </returns>
+    Task<IEnumerable<Order>> GetWithProductIdAsync(Guid productId);
+
+    /// <summary>
+    /// Adds order to the storage.
+    /// </summary>
+    /// <param name="order">The order to add.</param>
+    /// <returns>
+    /// The added <see cref="Order"/> if adding is successful;
+    /// otherwise, <c>null</c>.
+    /// </returns>
+    Task<Order?> AddOrderAsync(Order order);
+
+    /// <summary>
+    /// Updates order in the storage.
+    /// </summary>
+    /// <param name="order">The updated order.</param>
+    /// <returns>
+    /// The updated <see cref="Order"/> if updation is successful;
+    /// otherwise, <c>null</c>.
+    /// </returns>
+    Task<Order?> UpdateOrderAsync(Order order);
+
+    /// <summary>
+    /// Deletes order with the specified ID from the storage.
+    /// </summary>
+    /// <param name="id">The unique identifier of the order.</param>
+    /// <returns>
+    /// <c>true</c> if delition is successful;
+    /// otherwise, <c>false.</c>
+    /// </returns>
+    Task<bool> DeleteOrderAsync(Guid id);
+}
